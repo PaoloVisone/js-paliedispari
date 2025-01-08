9ba8f50881360987397694a5d3511caee441a724
PaoloVisone <polvis.pv.22@gmail.com>
2025-01-08 17:06:38 +0100

    Fix join, add if statement

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 // chiedere all'utente di inserire una parola
 let parolaUser = prompt("Inserisci una parola");
 console.log(isPalindroma(parolaUser))
+
 // funzione che controlla se la parola è palindroma
 function isPalindroma(parolaUser) {
 
-    // Creo un array con le lettere della parola inserita
+    // creo un array con le lettere della parola inserita
     let splitWord = parolaUser.split('');
     console.log(splitWord);
 
-    // Uso reverse per invertire le lettere
+    // uso reverse per invertire le lettere
     let reverseWord = splitWord.reverse();
     console.log(reverseWord);
 
     // unisco le lettere della parola con join
-    let joinWord = reverseWord.join(' ');
+    let joinWord = reverseWord.join('');
     console.log(joinWord);
 
     // verifico se la parola è palindroma
-
+    if (joinWord === parolaUser) {
+        return true;
+    } else {
+        return false;
+    }
 }
